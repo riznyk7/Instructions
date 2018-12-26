bd77a31ff8d019bbfb482c6e81d1b2348b55907b
riznyk7 <riznyk7@gmail.com>
2018-12-26 18:19:58 +0100

    no message

--- Sources/Instructions/Extra/Default Views/CoachMarkBodyDefaultView.swift
+++ Sources/Instructions/Extra/Default Views/CoachMarkBodyDefaultView.swift
@@ -35,45 +35,45 @@ public class CoachMarkBodyDefaultView: UIControl, CoachMarkBodyView {
             } else {
                 self.views.backgroundImageView.image = self.views.backgroundImage
             }
-
+            
             self.highlightArrowDelegate?.highlightArrow(self.isHighlighted)
         }
     }
-
+    
     public var nextControl: UIControl? {
         return self
     }
-
+    
     public var nextLabel: UILabel { return views.nextLabel }
     public var hintLabel: UITextView { return views.hintLabel }
-
+    
     public weak var highlightArrowDelegate: CoachMarkBodyHighlightArrowDelegate?
-
+    
     fileprivate var views = CoachMarkBodyDefaultViewHolder()
-
+    
     // MARK: - Initialization
     override public init(frame: CGRect) {
         super.init(frame: frame)
         setupAccessibilityIdentifier()
-
+        
         let helper = CoachMarkBodyDefaultViewHelper()
-
+        
         self.setupInnerViewHierarchy(using: helper)
     }
-
+    
     convenience public init() {
         self.init(frame: CGRect.zero)
     }
-
+    
     required public init?(coder aDecoder: NSCoder) {
         fatalError("This class does not support NSCoding.")
     }
-
+    
     public init(frame: CGRect, hintText: String, nextText: String?) {
         super.init(frame: frame)
-
+        
         let helper = CoachMarkBodyDefaultViewHelper()
-
+        
         if let next = nextText {
             views.hintLabel.text = hintText
             views.nextLabel.text = next
@@ -83,7 +83,7 @@ public class CoachMarkBodyDefaultView: UIControl, CoachMarkBodyView {
             setupSimpleInnerViewHierarchy(using: helper)
         }
     }
-
+    
     convenience public init(hintText: String, nextText: String?) {
         self.init(frame: CGRect.zero, hintText: hintText, nextText: nextText)
     }
@@ -94,27 +94,25 @@ private extension CoachMarkBodyDefaultView {
     //Configure the CoachMark view with a hint message and a next message
     func setupInnerViewHierarchy(using helper: CoachMarkBodyDefaultViewHelper) {
         translatesAutoresizingMaskIntoConstraints = false
-
-        helper.configureBackground(self.views.backgroundImageView, addTo: self)
         helper.configureHint(hintLabel, addTo: self)
-        helper.configureNext(nextLabel, addTo: self)
-        helper.configureSeparator(self.views.separator, addTo: self)
-
-        let views = (hintLabel: self.views.hintLabel, nextLabel: self.views.nextLabel,
-                     separator: self.views.separator)
-
-        self.addConstraints(helper.makeHorizontalConstraints(for: views))
+        
+        self.views.hintLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .medium)
+        self.views.hintLabel.textColor = .black
+        NSLayoutConstraint.activate([
+            self.views.hintLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.views.hintLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+            ])
     }
-
+    
     func setupSimpleInnerViewHierarchy(using helper: CoachMarkBodyDefaultViewHelper) {
         translatesAutoresizingMaskIntoConstraints = false
-
+        
         let helper = CoachMarkBodyDefaultViewHelper()
-
+        
         helper.configureBackground(self.views.backgroundImageView, addTo: self)
         helper.configureSimpleHint(hintLabel, addTo: self)
     }
-
+    
     func setupAccessibilityIdentifier() {
         accessibilityIdentifier = AccessibilityIdentifiers.coachMarkBody
     }
@@ -127,21 +125,21 @@ private struct CoachMarkBodyDefaultViewHolder {
         nextLabel.accessibilityIdentifier = AccessibilityIdentifiers.coachMarkNext
         return nextLabel
     }()
-
+    
     let hintLabel: UITextView = {
         let nextLabel = UITextView()
         nextLabel.accessibilityIdentifier = AccessibilityIdentifiers.coachMarkHint
         return nextLabel
     }()
-
+    
     let separator = UIView()
-
+    
     lazy var backgroundImageView: UIImageView = {
         return UIImageView(image: self.backgroundImage)
     }()
-
+    
     let backgroundImage = UIImage(namedInInstructions: "background")
     let highlightedBackgroundImage = UIImage(namedInInstructions: "background-highlighted")
-
+    
     init() { }
 }
